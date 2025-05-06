5266a09c9cc7a606ac284693c58bb0dfd2f8e9d9
Tobi <tobi@kinoptikon.de>
2025-05-06 18:56:58 +0200

    bold text

--- web.py
+++ web.py
@@ -3,18 +3,20 @@ import functions
 
 todos = functions.get_todos()
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
-st.text_input(label="", placeholder="Enter a to-do",
-              on_change=add_todo, key="new_todo")
 
 st.title("My Todos")
 st.subheader("Get more done every day.")
 st.write("Type away to <b>get started!</b>",
          unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Enter a to-do",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
